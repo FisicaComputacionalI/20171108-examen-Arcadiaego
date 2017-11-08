a5a3f0e1278b948f0cbc4938ee28945f0137a50c
Arcadiaego <31354205+Arcadiaego@users.noreply.github.com>
2017-11-08 14:00:39 -0600

    Add files via upload

---
+++ velocidad.cc
@@ -0,0 +1,24 @@
+/*
+Autor:	Faustino Moiśes Amador García
+Fecha: 	8 de Noviembre de 2017
+Exámen
+*/
+
+#include <iostream>
+using namespace std;
+
+int main (){
+	float V, D, T;
+	while (T!=0){
+	if (T!=0){
+	cout<<"Introduce valor de D"<<D<<endl;
+	cin>>D;
+	cout<<"Introduce valor de T"<<T<<endl;
+	cin>>T;
+		V=D/T;	
+	}	
+	else if (T=0)
+		break;
+	}
+}
+	
