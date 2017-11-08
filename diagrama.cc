4502bf018644c7be2d0af9f9a80f4654a5784112
Arcadiaego <31354205+Arcadiaego@users.noreply.github.com>
2017-11-08 13:41:59 -0600

    Codigo del diagrama de flujo

---
+++ diagrama.cc
@@ -0,0 +1,33 @@
+/*
+Autor:	Faustino Moiśes Amador García
+Fecha: 	8 de Noviembre de 2017
+Exámen
+*/
+
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	int x=1;
+	while(x<=5){
+		if(x%3==0)
+			x=x+2;
+			if (x%3!=0) 
+			cout<<"El valor de x es "<<x<<endl;
+			x=x+2;
+		if(x>5)
+		break;	
+	}
+		
+	return 0;
+}
+
+
+/*
+El programa en automatico recibe el valor de x, luego lo procesa de la siguente forma:
+si es menor que 5 entonces pregunta si la división de x entre 3 es exacta si lo es entonces solo le suma dos, de lo contrario imprime el valor y despues le suma dos, de forma que al hacer el ciclo el valor de x es mayor que 5 haciendo que el programa salga.
+imprimiendo los siguientes valores
+el valor de x es 1
+el valor de x es 5
+*/
